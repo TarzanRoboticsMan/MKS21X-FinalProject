a17ac41985ba11b329b2b3d4b9d57936efbd558b
TarzanRoboticsMan <dsontag00@stuy.edu>
2019-01-04 11:40:20 -0500

    Added prototype dict, wrote WordLadder to read it

---
+++ WordLadder.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WordLadder{
+  public static void main(String[]args){
+    String filename = "prototypeDict.txt"
+    try{
+      File f = new File(filename);
+      Scanner dict = new Scanner(f);
+
+        //NOW read the file here...
+
+    }catch(FileNotFoundException e){
+      System.out.println("File not found: " + filename);
+      //e.printStackTrace();
+      System.exit(1);
+    }
+  }
+}
