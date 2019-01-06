673010a2fe6ce15d1e48f09df7f3d5f281aa52ed
Isabella Rocha <irocha00@stuy.edu>
2019-01-05 23:39:28 -0500

    Wrote basic code for our Nodes

--- Node.java
+++ Node.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Node {
   private String value;
-  private ArrayList<Node> previous;
+  private ArrayList<Node> previous; //List of previouses as multiple paths might lead to the same word
 
   public Node(String value) {
     this.value = value;
@@ -13,7 +13,7 @@ public class Node {
   public ArrayList<Node> getPrevious() {
     return previous;
   }
-  public void setPrevious(String value){
-
+  public void setPrevious(Node str){
+    previous.add(str);
   }
 }
