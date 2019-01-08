b22926d15d9400a682b0756937ecdcc832a432b5
Isabella Rocha <irocha00@stuy.edu>
2019-01-07 23:20:34 -0500

    Wrote isWord and driver for testing

--- Dictionary.java
+++ Dictionary.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Dictionary{
-  private ArrayList<String> wordList;
+  private ArrayList<String> wordList = new ArrayList<String>();
   private int[] tableOfContents;
 
   public Dictionary(){
     try{
-      File f = new File("wordlist.txt");
+      File f = new File("wordList.txt");
       Scanner dict = new Scanner(f);
       while (dict.hasNext()) {
-        wordList.add(dict.next());
+        wordList.add(dict.next().toLowerCase()); //Loops through and adds words from dictionary to an ArrayList
       }
     }catch(FileNotFoundException e){
-      System.out.println("File not found: wordlist.txt");
+      System.out.println("File not found: wordList.txt");
       System.exit(1);
     }
     tableOfContents = new int[26]; //Creating a list of the indexes at which the a words start, b words, c words, etx. to quickly access words
@@ -23,18 +23,36 @@ public class Dictionary{
     tableOfContents[0] = 0;
     for(int idx = 1; idx < wordList.size(); idx++) {
       if(wordList.get(idx).charAt(0) != (wordList.get(idx - 1).charAt(0))){
-        tableOfContents[x] = idx;
+        tableOfContents[x] = idx + 1;
         x++;
       }
     }
   }
   public boolean isWord(String value){
     value = value.toLowerCase();
-    for (int idx = value.charAt(0) - 97; idx < tableOfContents[value.charAt(0) - 96]; idx ++) { //Loops through wordList but only between the words that share the same first letter
-      if (wordList.get(idx) == value) {
+    for (int idx = tableOfContents[value.charAt(0) - 97]; idx < tableOfContents[value.charAt(0) - 96]; idx ++) { //Loops through wordList but only between the words that share the same first letter
+      if (wordList.get(idx).equals(value)) {
         return true;
       }
     }
     return false;
+  }
+  public ArrayList<String> retrieve() { //To access wordList while testing
+    return wordList;
+  }
+  public static void main(String args[]) { //Driver for testing code
+    Dictionary dic = new Dictionary();
+    for (int idx = 0; idx < 20; idx++) {
+      System.out.println(dic.retrieve().get(idx));
+    }
+    boolean check1 = dic.isWord("apple");
+    boolean check2 = dic.isWord("APPLE");
+    boolean check3 = dic.isWord("deaedfs");
+    boolean check4 = dic.isWord("xylophone");
+    System.out.println(dic.retrieve().get(3839));
+    System.out.println(check1 + "(Should print true)");
+    System.out.println(check2 + "(Should print true)");
+    System.out.println(check3 + "(Should print false)");
+    System.out.println(check4 + "(Should print true)");
   }
 }
--- README.md
+++ README.md
@@ -3,12 +3,17 @@ Final Project Name: WordLadder
 Group Name: Socha Word Olympic Team
 
 
-
-Izzy, Log 1:
-  I wrote the basic code for the Node class in which we will store our words and their previous words to maintain their "path". I also downloaded a word list text file to use as our dictionary. I wrote the constructor for the dictionary class, creating the ArrayList of words based off of code Coby wrote earlier and then wrote the code to develop the table of contents, allowing us to more quickly access words based off of their first letter so we don't have to loop through the entire file everytime.
-
 Coby, 1/4:
   Wrote prototype dictionary and scanner in WordLadder.java to test scanning through words. Spent a bunch of time studying javadocs for scanner and patterns
 
-Izzy, Log 2:
+
+Izzy, 1/5:
+  I wrote the basic code for the Node class in which we will store our words and their previous words to maintain their "path". I also downloaded a word list text file to use as our dictionary. I wrote the constructor for the dictionary class, creating the ArrayList of words based off of code Coby wrote earlier and then wrote the code to develop the table of contents, allowing us to more quickly access words based off of their first letter so we don't have to loop through the entire file everytime.
+
+
+Izzy, 1/6:
   Didn't do a lot today, mostly tried to figure out how to get lanterna and the TerminalDemo provided in class to work on windows, finally got it to work thanks to Ethan Morgan who helped a lot by providing the other version of lanterna and a rewritten form of TerminalDemo.java. Then just edited Dictionary.java to fix the code that created the table of contents and fixed a typo in wordLadder.
+
+
+Izzy, 1/7:
+  Got a working dictionary class that quickly stores all the words in an ArrayList and then can accurately tell whether or not a word is a real word or not using isWord. 
