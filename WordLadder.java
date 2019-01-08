246d0674f20f0b1f3531225014e2d3f13e9feb64
Isabella Rocha <irocha00@stuy.edu>
2019-01-08 18:11:33 -0500

    Wrote oneOff such that it works with Strings, not nodes yet

--- WordLadder.java
+++ WordLadder.java
@@ -15,12 +15,12 @@ public class WordLadder{
     for (int x = 0; x < val.length(); x++) { //Going through all the possibilities one position at a time
       for (int idx = 97; idx < 123; idx++) { //Switching it to every possible letter
         String begin = val.substring(0, x);
-        int mid = val.charAt(x) + idx;
-        String end = val.substring(x + 1);
-        val = begin + (char)mid + end; //Creating new word with changed letter
-        if (dic.isWord(val)) {
+        String end = val.substring(x + 1, val.length());
+        String val2;
+        val2 = begin + (char)idx + end; //Creating new word with changed letter
+        if (dic.isWord(val2) && !val2.equals(val)) {
     //      Node input = new Node(val);
-          output.add(val); //Adding it to our ArrayList of words that are one away if it is a valid word
+          output.add(val2); //Adding it to our ArrayList of words that are one away if it is a valid word
         }
       }
     }
@@ -31,5 +31,6 @@ public class WordLadder{
     System.out.println(test.oneOff("apple").toString());
     System.out.println(test.oneOff("hello").toString());
     System.out.println(test.oneOff("xylophone").toString());
+    System.out.println(test.oneOff("bad"));
   }
 }
